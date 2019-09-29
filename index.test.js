9f540fb5ffa053d55bc7867ff8d72fd1e712cad2
LSI <lsi@globocenter.by>
2019-09-29 10:42:09 +0300

    feat: implement a solution

--- index.test.js
+++ index.test.js
@@ -31,141 +31,141 @@ const talentedStudentSpeedConfig = {
 describe('js-edu', () => {
     describe('for general student with programming skills', () => {
         it('should could take 200 weeks for family focus', () => {
-            const weeks = getTimeForEducation('family', true,  defaultStudentSpeedConfig);
-    
+            const weeks = getTimeForEducation('family', true, defaultStudentSpeedConfig);
+
             expect(weeks).to.equal(200);
         });
         it('should could take 80 weeks for friends focus', () => {
-            const weeks = getTimeForEducation('friends', true,  defaultStudentSpeedConfig);
-    
+            const weeks = getTimeForEducation('friends', true, defaultStudentSpeedConfig);
+
             expect(weeks).to.equal(80);
         });
         it('should could take 40 weeks for normal_life focus', () => {
-            const weeks = getTimeForEducation('normal_life', true,  defaultStudentSpeedConfig);
-    
+            const weeks = getTimeForEducation('normal_life', true, defaultStudentSpeedConfig);
+
             expect(weeks).to.equal(40);
         });
         it('should could take 27 weeks for profession focus', () => {
-            const weeks = getTimeForEducation('profession', true,  defaultStudentSpeedConfig);
-    
+            const weeks = getTimeForEducation('profession', true, defaultStudentSpeedConfig);
+
             expect(weeks).to.equal(27);
         });
         it('should could take 20 weeks for carrier focus', () => {
-            const weeks = getTimeForEducation('carrier', true,  defaultStudentSpeedConfig);
-    
+            const weeks = getTimeForEducation('carrier', true, defaultStudentSpeedConfig);
+
             expect(weeks).to.equal(20);
         });
         it('should could take 14 weeks for top_peformance focus', () => {
-            const weeks = getTimeForEducation('top_peformance', true,  defaultStudentSpeedConfig);
-    
+            const weeks = getTimeForEducation('top_peformance', true, defaultStudentSpeedConfig);
+
             expect(weeks).to.equal(14);
         });
     });
     describe('for problem student with programming skills', () => {
         const studentConfig = problemStudentSpeedConfig;
         it('should could take a lot of weeks for family focus', () => {
-            const weeks = getTimeForEducation('family', true,  studentConfig);
-    
+            const weeks = getTimeForEducation('family', true, studentConfig);
+
             expect(weeks).to.equal(400);
         });
         it('should could take less weeks for friends focus', () => {
-            const weeks = getTimeForEducation('friends', true,  studentConfig);
-    
+            const weeks = getTimeForEducation('friends', true, studentConfig);
+
             expect(weeks).to.equal(200);
         });
         it('should could take normal number of weeks for normal_life focus', () => {
-            const weeks = getTimeForEducation('normal_life', true,  studentConfig);
-    
+            const weeks = getTimeForEducation('normal_life', true, studentConfig);
+
             expect(weeks).to.equal(100);
         });
         it('should could take optimal number of weeks for profession focus', () => {
-            const weeks = getTimeForEducation('profession', true,  studentConfig);
-    
+            const weeks = getTimeForEducation('profession', true, studentConfig);
+
             expect(weeks).to.equal(40);
         });
         it('should could take a few weeks for carrier focus', () => {
-            const weeks = getTimeForEducation('carrier', true,  studentConfig);
-    
+            const weeks = getTimeForEducation('carrier', true, studentConfig);
+
             expect(weeks).to.equal(27);
         });
     });
     describe('for talented student with programming skills', () => {
         const studentConfig = talentedStudentSpeedConfig;
-        
+
         it('should could take normal number of weeks for normal_life focus', () => {
-            const weeks = getTimeForEducation('normal_life', true,  studentConfig);
-    
+            const weeks = getTimeForEducation('normal_life', true, studentConfig);
+
             expect(weeks).to.equal(27);
         });
         it('should could take optimal number of weeks for profession focus', () => {
-            const weeks = getTimeForEducation('profession', true,  studentConfig);
-    
+            const weeks = getTimeForEducation('profession', true, studentConfig);
+
             expect(weeks).to.equal(20);
         });
         it('should could take a few weeks for carrier focus', () => {
-            const weeks = getTimeForEducation('carrier', true,  studentConfig);
-    
+            const weeks = getTimeForEducation('carrier', true, studentConfig);
+
             expect(weeks).to.equal(15);
         });
         it('should could take 14 weeks for top_peformance focus', () => {
-            const weeks = getTimeForEducation('top_peformance', true,  studentConfig);
-    
+            const weeks = getTimeForEducation('top_peformance', true, studentConfig);
+
             expect(weeks).to.equal(12);
         });
     });
     describe('for general student without programming skills', () => {
         const studentConfig = defaultStudentSpeedConfig;
         it('should could take a lot of weeks for family focus', () => {
-            const weeks = getTimeForEducation('family', false,  studentConfig);
-    
+            const weeks = getTimeForEducation('family', false, studentConfig);
+
             expect(weeks).to.equal(325);
         });
         it('should could take less weeks for friends focus', () => {
-            const weeks = getTimeForEducation('friends', false,  studentConfig);
-    
+            const weeks = getTimeForEducation('friends', false, studentConfig);
+
             expect(weeks).to.equal(130);
         });
         it('should could take normal number of weeks for normal_life focus', () => {
-            const weeks = getTimeForEducation('normal_life', false,  studentConfig);
-    
+            const weeks = getTimeForEducation('normal_life', false, studentConfig);
+
             expect(weeks).to.equal(65);
         });
         it('should could take optimal number of weeks for profession focus', () => {
-            const weeks = getTimeForEducation('profession', false,  studentConfig);
-    
+            const weeks = getTimeForEducation('profession', false, studentConfig);
+
             expect(weeks).to.equal(44);
         });
         it('should could take a few weeks for carrier focus', () => {
-            const weeks = getTimeForEducation('carrier', false,  studentConfig);
-    
+            const weeks = getTimeForEducation('carrier', false, studentConfig);
+
             expect(weeks).to.equal(33);
         });
     });
     describe('for problem student without programming skills', () => {
         const studentConfig = problemStudentSpeedConfig;
         it('should could take a lot of weeks for family focus', () => {
-            const weeks = getTimeForEducation('family', false,  studentConfig);
-    
+            const weeks = getTimeForEducation('family', false, studentConfig);
+
             expect(weeks).to.equal(650);
         });
         it('should could take less weeks for friends focus', () => {
-            const weeks = getTimeForEducation('friends', false,  studentConfig);
-    
+            const weeks = getTimeForEducation('friends', false, studentConfig);
+
             expect(weeks).to.equal(325);
         });
         it('should could take normal number of weeks for normal_life focus', () => {
-            const weeks = getTimeForEducation('normal_life', false,  studentConfig);
-    
+            const weeks = getTimeForEducation('normal_life', false, studentConfig);
+
             expect(weeks).to.equal(163);
         });
         it('should could take optimal number of weeks for profession focus', () => {
-            const weeks = getTimeForEducation('profession', false,  studentConfig);
-    
+            const weeks = getTimeForEducation('profession', false, studentConfig);
+
             expect(weeks).to.equal(65);
         });
         it('should could take a few weeks for carrier focus', () => {
-            const weeks = getTimeForEducation('carrier', false,  studentConfig);
-    
+            const weeks = getTimeForEducation('carrier', false, studentConfig);
+
             expect(weeks).to.equal(44);
         });
     });
--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
+
+const speedConfig = {
+    family: 4,
+    friends: 10,
+    normal_life: 20,
+    profession: 30,
+    carrier: 40,
+    top_peformance: 60
+}
+const knowsProgramming = true;
+
 module.exports = function getTimeForEducation(
-    focus = 'family', 
+    focus = 'family',
     knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+    config = { family: 4 }
+) {
+    const courseHour = 800;
+    const coefWithoutKnowledge = 1.625;
+    let cursWeeks = Math.ceil(courseHour / config[focus]);
+    if (!knowsProgramming) {
+        cursWeeks = Math.ceil(cursWeeks * coefWithoutKnowledge);
+    }
+    return cursWeeks;
+};
